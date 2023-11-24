e0a977a6cb757819a95c9238dbd3d154daf2f6ca
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-11-24 17:26:41 +0100

    Debugging continued.

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -87,8 +87,10 @@ mkdir -p $gendoc/docs
 [[ -h $gendoc/docs/assets ]]      || create_link $repodir/docs/assets      $repodir/$gendoc/docs/assets
 [[ -h $gendoc/docs/stylesheets ]] || create_link $repodir/docs/stylesheets $repodir/$gendoc/docs/stylesheets
 
-for file in $(find docs -maxdepth 1 -name "*.md")
+echo "Searching for files in repo doc."
+for file in $(find docs -mindepth 1 -maxdepth 1 -name "*.md")
 do
+    echo "Found file $file."
     [[ -h $gendoc/$file ]] || create_link $repodir/$file $repodir/$gendoc/$file
 done
 
