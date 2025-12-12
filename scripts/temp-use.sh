93a73ca534d8ba3e370f73545dc0ccf0f0a09fef
Kurt Lust <Kurt.Lust@uantwerpen.be>
2025-12-12 10:09:36 +0100

    Adding a README explaining the newly added functionality.

--- scripts/temp-use.sh
+++
@@ -1,7 +0,0 @@
-selecttc buildtools-24.03.eb result ; echo $result
-
-for file in $(find . -name "*.eb") ; do selecttc $file result ; echo "$file: $result"; done
-
-
-containeronly=('24.11', '25.03')
-if [[ " ${containeronly[*]} " =~ " $result " ]] ; then echo "Container only!" ; fi
